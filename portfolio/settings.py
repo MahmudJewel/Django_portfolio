12fd1dc8d7cf590115da4f0248bc57aa99338498
villain <villain@kali.local>
2021-08-14 01:03:31 +0600

    configuration for live server

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wx-ps=o6gm)gbmy#&=9sjk4qxq9)sawcqz@ju5!6q794-7e&+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
