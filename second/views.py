79b604cbf4a78e348f93646fb1276f25bc95ce66
jewel <jmbengal.mbstu@gmail.com>
2022-01-25 01:09:30 +0600

    Bootstrap projects are added

--- second/views.py
+++ second/views.py
@@ -33,115 +33,140 @@ def portfolio(request):
 	djShoppingMall=projects()
 	djShoppingMall.link= "https://github.com/MahmudJewel/E_commerce"
 	djShoppingMall.title="Shopping Mall"
-	djShoppingMall.desc="Django Project"
+	djShoppingMall.desc="Django"
 	djShoppingMall.img='../static/img/project-img/dj-shoppingMall.jpg'
 
 	djBlog=projects()
 	djBlog.link= "https://github.com/MahmudJewel/techvillain"
-	djBlog.title="Personal Blog(tech villain)"
-	djBlog.desc="Django Project"
+	djBlog.title="Blog site"
+	djBlog.desc="Django"
 	djBlog.img='../static/img/project-img/dj-blog.jpg'
 
 	djGrocery=projects()
 	djGrocery.link= "https://github.com/MahmudJewel/Juwel-Mahmud_iTQAN-Django-Task"
 	djGrocery.title="Grocery Shop"
-	djGrocery.desc="Django Project"
+	djGrocery.desc="Django"
 	djGrocery.img='../static/img/project-img/dj-grocery1.jpg'
 
 	djOnlineExam=projects()
 	djOnlineExam.link= "https://github.com/MahmudJewel/online_exam"
 	djOnlineExam.title="Online Exam"
-	djOnlineExam.desc="Django Project"
+	djOnlineExam.desc="Django"
 	djOnlineExam.img='../static/img/project-img/dj-onlineExam.jpg'
 
 	djPortfolio=projects()
 	djPortfolio.link= "https://github.com/MahmudJewel/portfolioWithDjango"
 	djPortfolio.title="Portfolio-Django"
-	djPortfolio.desc="Django Project"
+	djPortfolio.desc="Django"
 	djPortfolio.img='../static/img/project-img/dj-portfolio.jpg'
 
 	djWeather=projects()
 	djWeather.link= "https://github.com/MahmudJewel/Weather"
 	djWeather.title="Weather Detector"
-	djWeather.desc="Django Project"
+	djWeather.desc="Django"
 	djWeather.img='../static/img/project-img/dj-weather.png'
 
 	djTodo=projects()
 	djTodo.link= "https://github.com/MahmudJewel/ToDoApps"
 	djTodo.title="ToDoApps"
-	djTodo.desc="Django Project"
+	djTodo.desc="Django"
 	djTodo.img='../static/img/project-img/dj-todo.jpg'
 
 	djTakingNotes=projects()
 	djTakingNotes.link= "https://github.com/MahmudJewel/notes-taking"
 	djTakingNotes.title="Taking Notes"
-	djTakingNotes.desc="Django Project"
+	djTakingNotes.desc="Django"
 	djTakingNotes.img='../static/img/project-img/dj-takingnotes.jpg'
 
 	djStudentForm=projects()
 	djStudentForm.link= "https://github.com/MahmudJewel/studentForm"
 	djStudentForm.title="Student Form"
-	djStudentForm.desc="Django Project"
+	djStudentForm.desc="Django"
 	djStudentForm.img='../static/img/project-img/dj-st.jpg'
 
 
 	djFamouseQuotes=projects()
 	djFamouseQuotes.link= "https://github.com/MahmudJewel/Quotes"
 	djFamouseQuotes.title="Famous Quotes"
-	djFamouseQuotes.desc="Django Project"
+	djFamouseQuotes.desc="Django"
 	djFamouseQuotes.img='../static/img/project-img/dj-quotes.jpg'
 
 	# react projects 
 	reactCal=projects()
 	reactCal.link= "https://github.com/MahmudJewel/react-calculator"
 	reactCal.title="React Calculator"
-	reactCal.desc="React Project"
+	reactCal.desc="React"
 	reactCal.img='../static/img/project-img/react-calculator.jpg'
 
 	reactForm=projects()
 	reactForm.link= "https://github.com/MahmudJewel/react-form"
 	reactForm.title="React Form"
-	reactForm.desc="React Project"
+	reactForm.desc="React"
 	reactForm.img='../static/img/project-img/react-form.jpg'
+
+	# Bootstrap projects 
+	btEcommerce=projects()
+	btEcommerce.link= "https://github.com/MahmudJewel/E_commerce-frontend"
+	btEcommerce.title="E-commerce"
+	btEcommerce.desc="Bootstrap"
+	btEcommerce.img='../static/img/project-img/bt-shoppingmall.png'
+
+	btBlog=projects()
+	btBlog.link= "https://github.com/MahmudJewel/techvillain-frontend"
+	btBlog.title="Blog site"
+	btBlog.desc="Bootstrap"
+	btBlog.img='../static/img/project-img/bt-techVillain.png'
+
+	btGrocery=projects()
+	btGrocery.link= "https://github.com/MahmudJewel/Juwel-Mahmud_iTQAN-Django-Task"
+	btGrocery.title="Grocery Shop"
+	btGrocery.desc="Bootstrap"
+	btGrocery.img='../static/img/project-img/bt-groceryShop.png'
+
+	btOnlineExam=projects()
+	btOnlineExam.link= "https://github.com/MahmudJewel/online_exam_screenshot"
+	btOnlineExam.title="Exam Portal"
+	btOnlineExam.desc="Bootstrap"
+	btOnlineExam.img='../static/img/project-img/bt-onlineExam.png'
 
 	# javascript projects 
 	jsPortfolio=projects()
 	jsPortfolio.link= "https://github.com/MahmudJewel/portfolio"
 	jsPortfolio.title="Portfolio"
-	jsPortfolio.desc="JavaScript Project"
+	jsPortfolio.desc="JavaScript"
 	jsPortfolio.img='../static/img/project-img/js-portfolio.jpg'
 
 	jsRandomQuotes=projects()
 	jsRandomQuotes.link= "https://mahmudjewel.github.io/Random-Quotes/"
 	jsRandomQuotes.title="Random Quotes Generator"
-	jsRandomQuotes.desc="JavaScript Project"
+	jsRandomQuotes.desc="JavaScript"
 	jsRandomQuotes.img='../static/img/project-img/js-rq.jpg'
 
 
 	jsMTable=projects()
 	jsMTable.link= "https://mahmudjewel.github.io/Multplication-Table/"
 	jsMTable.title="Multiplication Table"
-	jsMTable.desc="JavaScript Project"
+	jsMTable.desc="JavaScript"
 	jsMTable.img='../static/img/project-img/js-mt.jpg'
 
 	jsBookList=projects()
 	jsBookList.link= "https://mahmudjewel.github.io/Book-List/"
 	jsBookList.title="Book List"
-	jsBookList.desc="JavaScript Project"
+	jsBookList.desc="JavaScript"
 	jsBookList.img='../static/img/project-img/js-booklist.jpg'
 
 	# java projects 
 	jvPharmacyStore=projects()
 	jvPharmacyStore.link= "https://github.com/MahmudJewel/Pharmacy-Store-Management-System"
 	jvPharmacyStore.title="Pharmacy Store Management System"
-	jvPharmacyStore.desc="Java Project"
+	jvPharmacyStore.desc="Java"
 	jvPharmacyStore.img='../static/img/project-img/jv-pharmacy.jpg'
 
 
 	jvFileTransfer=projects()
 	jvFileTransfer.link= "https://github.com/MahmudJewel/File-transferring-app"
 	jvFileTransfer.title="File Transferring app"
-	jvFileTransfer.desc="Java Project"
+	jvFileTransfer.desc="Java"
 	jvFileTransfer.img='../static/img/project-img/jv-file-transferring.jpg'
 
 
@@ -169,7 +194,12 @@ def portfolio(request):
 			jsRandomQuotes,
 			jsMTable
 		]
-	bootstrap_project_list=[]
+	bootstrap_project_list=[
+		btEcommerce,
+		btBlog,
+		btGrocery,
+		btOnlineExam,
+	]
  
 	java_project_list=[
 			jvPharmacyStore,
